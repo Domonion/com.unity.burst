44e1543f2d8ba2d3a92ddf5c962235bbe7d118dc
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2020-12-01 00:35:25 +0300

    initial

---
+++ Tests/Editor/SeparateAssembly/TypeHashWrapper.cs
@@ -0,0 +1,17 @@
+using Unity.Burst;
+
+//CGTD useless hashes
+public static class TypeHashWrapper
+{
+    public static int GetIntHash()
+    {
+        return BurstRuntime.GetHashCode32<int>();
+    }
+
+    public static int GetGenericHash<T>()
+    {
+        return BurstRuntime.GetHashCode32<SomeStruct<T>>();
+    }
+
+    public struct SomeStruct<T> { }
+}
---
+++ Tests/Runtime/FunctionPointerTests.cs
@@ -0,0 +1,24 @@
+using System;
+using NUnit.Framework;
+using Unity.Burst;
+
+[TestFixture, BurstCompile]
+public class FunctionPointerTests
+{
+    [BurstCompile(CompileSynchronously = true)]
+    private static T StaticFunctionNoArgsGenericReturnType<T>()
+    {
+        return default;
+    }
+
+    private delegate int DelegateNoArgsIntReturnType();
+
+    [Test]
+    public void TestCompileFunctionPointerNoArgsGenericReturnType()
+    {
+        //CGTD wtf
+        Assert.Throws<InvalidOperationException>(
+            () => BurstCompiler.CompileFunctionPointer<DelegateNoArgsIntReturnType>(StaticFunctionNoArgsGenericReturnType<int>),
+            "The method `Int32 StaticFunctionNoArgsGenericReturnType[Int32]()` must be a non-generic method");
+    }
+}
